857c0be14b6cec169aeabc469f190405927acdd9
cog <cogcoast@gmail.com>
2024-11-27 15:14:09 +0800

    add fn post_gcd

--- main.rs
+++ main.rs
@@ -14,3 +14,20 @@ fn gcd(mut n: u64, mut m: u64) -> u64 {
     }
     n
 }
+
+async fn post_gcd(form: web::Form<GcdParameters>) -> HttpResponse {
+    if form.n == 0 || form.m == 0 {
+        return HttpResponse::BadRequest()
+            .content_type("text/html")
+            .body("Computing the GCD with zero is boring.");
+    }
+
+    let response =
+        format!("The greatest common divisor of the numbers {} and {} \
+                 is <b>{}</b>\n",
+                form.n, form.m, gcd(form.n, form.m));
+
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(response)
+}
