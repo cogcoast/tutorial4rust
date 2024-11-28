d12841e43e6a53e5c743f62a8ddec7e2808a3471
cog <cogcoast@gmail.com>
2024-11-28 15:11:52 +0800

    add fn copy_dir_to

--- main.rs
+++ main.rs
@@ -31,3 +31,17 @@ async fn post_gcd(form: web::Form<GcdParameters>) -> HttpResponse {
         .content_type("text/html")
         .body(response)
 }
+
+fn copy_dir_to(src: &Path, dst: &Path) -> io::Result<()> {
+    if !dst.is_dir() {
+        fs::create_dir(dst)?;
+    }
+
+    for entry_result in src.read_dir()? {
+        let entry = entry_result?;
+        let file_type = entry.file_type()?;
+        copy_to(&entry.path(), &file_type, &dst.join(entry.file_name()))?;
+    }
+
+    Ok(())
+}
