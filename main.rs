0309bd6c47faa20fab4204eeac119ef8e376bac8
cog <cogcoast@gmail.com>
2024-12-01 10:15:35 +0800

    add async fn post_gcd

--- main.rs
+++ main.rs
@@ -45,6 +45,23 @@ fn main() {
              numbers, d);
 }
 
+async fn post_gcd(form: web::Form<GcdParameters>) -> HttpResponse {
+    if form.n == 0 || form.m == 0 {
+        return HttpResponse::BadRequest()
+            .content_type("text/html")
+            .body("Computing the GCD with zero is boring.");
+    }
+
+    let response =
+        format!("The greatest common divisor of the numbers {} and {} \
+                 is <b>{}</b>\n",
+                form.n, form.m, gcd(form.n, form.m));
+
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(response)
+}
+
 use actix_web::{web, App, HttpResponse, HttpServer};
 
 #[actix_web::main]
