cefee58050b2c1e44e7288a958a65af5196ea297
cog <cogcoast@gmail.com>
2024-12-13 12:18:01 +0800

    add very_generic

--- main.rs
+++ main.rs
@@ -119,6 +119,25 @@ macro_rules! define_complex {
     };
 }
 
+mod very_generic {
+    define_complex!();
+
+    use std::ops::Add;
+
+    impl<L, R> Add<Complex<R>> for Complex<L>
+    where
+        L: Add<R>,
+    {
+        type Output = Complex<L::Output>;
+        fn add(self, rhs: Complex<R>) -> Self::Output {
+            Complex {
+                re: self.re + rhs.re,
+                im: self.im + rhs.im,
+            }
+        }
+    }
+}
+
 mod non_generic_add {
     define_complex!();
 
