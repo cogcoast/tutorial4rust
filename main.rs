94564343f1a02ea73da0bdaa6fda55d62d61a36a
cog <cogcoast@gmail.com>
2024-12-18 12:07:16 +0800

    add copy_dir_to

--- main.rs
+++ main.rs
@@ -173,6 +173,20 @@ mod derive_everything {
     }
 }
 
+fn copy_dir_to(src: &Path, dst: &Path) -> io::Result<()> {
+    if !dst.is_dir() {
+        fs::create_dir(dst)?;
+    }
+
+    for entry_result in src.read_dir()? {
+        let entry = entry_result?;
+        let file_type = entry.file_type()?;
+        copy_to(&entry.path(), &file_type, &dst.join(entry.file_name()))?;
+    }
+
+    Ok(())
+}
+
 fn complex() {
     #[derive(Copy, Clone, Debug)]
     struct Complex { re: f64, im: f64 }
